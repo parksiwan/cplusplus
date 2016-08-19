eebfff5eed7f693ddbd75061734169cc674d6841
parksiwan <parksiwan@gmail.com>
2016-08-19 13:46:54 +1000

    Add files via upload

---
+++ ex2_2.cpp
@@ -0,0 +1,17 @@
+// Using the same rule as the shapes programs from earlier in the chapter (only
+// two output statements—one that outputs the hash mark and one that outputs
+// an end - of - line and space), write a program that produces the following shape :
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 4; i > 0; i--) {
+        for (int j = 0; j < (4 - i); j++)
+	        cout << ' ';
+		for (int k = 0; k < (2 * i); k++)
+			cout << '#';
+	cout << endl;
+    }
+    return 1;
+}
