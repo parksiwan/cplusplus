ce5f17c027ed16bfda700c86d41fda25c92edd58
parksiwan <parksiwan@gmail.com>
2016-08-19 14:17:04 +1000

    Update ex2_1.cpp

--- ex2_1.cpp
+++ ex2_1.cpp
@@ -1,6 +1,10 @@
 // Using the same rule as the shapes programs from earlier in the chapter (only
 // two output statements—one that outputs the hash mark and one that outputs
 // an end - of - line and space), write a program that produces the following shape :
+//########
+// ######
+//  ####
+//   ##
 
 #include <iostream>
 using namespace std;
