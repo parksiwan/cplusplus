1376ca49bb4a0fc63ddf1dbe731e30dbc11e9ed9
parksiwan <parksiwan@users.noreply.github.com>
2016-08-23 17:03:32 +1000

    Create ex3_qsort.cpp

---
+++ ex3_qsort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int values[] = {40, 10, 100, 90, 20,25};
+char char_vals[] = {'c', 'f', 'r', 'h', 'a', 'e'};
+
+int IntCompare(const void* a, const void* b) {
+	return( *(int*)a - *(int*)b);
+}
+
+int CharCompare(const void* a, const void* b) {
+	return( *(char*)a - *(char*)b);
+}
+
+int main(int argc, char **argv) {
+	qsort(values, 6, sizeof(int), IntCompare);
+	qsort(char_vals, 6, sizeof(char), CharCompare);
+	
+	for (int i = 0; i < 6; i++)
+		cout << values[i] << '-';
+		
+	cout << endl;
+	for (int i = 0; i < 6; i++)
+		cout << char_vals[i] << '-';
+		
+	return 0;
+}
