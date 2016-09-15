fa70661e311c4868699f290c0f662a33070d82ce
parksiwan <parksiwan@users.noreply.github.com>
2016-09-16 08:57:19 +1000

    Update ex3_insertion_sort.cpp

--- ex3_insertion_sort.cpp
+++ ex3_insertion_sort.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
 	const int SIZE = 10;
 	int arr[SIZE] = {87, 28, 100, 78, 84, 98, 75, 70, 81, 68};
 	int start = 0, end = SIZE - 1;
@@ -11,8 +12,10 @@ int main(int argc, char **argv) {
 		cout << arr[i] << '-';
 	cout << endl;
 	
-	for (int i = start + 1; i <= end; i++) {
-		for (int j = i; j > start && arr[j-1] > arr[j]; j--) {
+	for (int i = start + 1; i <= end; i++) 
+	{
+		for (int j = i; j > start && arr[j-1] > arr[j]; j--) 
+		{
 			int temp = arr[j-1];
 			arr[j-1] = arr[j];
 			arr[j] = temp;
