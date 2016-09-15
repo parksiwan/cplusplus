cbcee2e8dd563ba2e07ea0e88f3f7d461a1a3d65
parksiwan <parksiwan@users.noreply.github.com>
2016-09-16 08:49:11 +1000

    Update ex2_2.cpp

--- ex2_2.cpp
+++ ex2_2.cpp
@@ -13,15 +13,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	for (int i = 1; i < 5; i++) {
+int main() 
+{
+	for (int i = 1; i < 5; i++) 
+	{
 		for (int j = 0; j < abs(4 - i); j++)
 			cout << ' ';
 		for (int k = 0; k < (2 * i); k++)
 			cout << '#';
 		cout << endl;
 	}
-	for (int i = 4; i > 0; i--) {
+	for (int i = 4; i > 0; i--) 
+	{
 		for (int j = 0; j < abs(4 - i); j++)
 			cout << ' ';
 		for (int k = 0; k < (2 * i); k++)
