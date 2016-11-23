8b10b6dbc2d4b5e0d9ab0c9e221115f00b8b3648
parksiwan <parksiwan@users.noreply.github.com>
2016-11-23 21:14:29 +1100

    Update ex2_1.cpp

--- ex2_1.cpp
+++ ex2_1.cpp
@@ -9,10 +9,8 @@
 #include <iostream>
 using namespace std;
 
-int main() 
-{
-	for (int i = 4; i > 0; i--) 
-	{
+int main() {
+	for (int i = 4; i > 0; i--) {
 		for (int j = 0; j < (4 - i); j++)
 			cout << ' ';
 		for (int k = 0; k < (2 * i); k++)
