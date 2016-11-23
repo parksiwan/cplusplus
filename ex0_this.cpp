140f451db78342b94e31d0933a4e24660a659ef0
parksiwan <parksiwan@users.noreply.github.com>
2016-11-23 21:13:56 +1100

    Update ex0_this.cpp

--- ex0_this.cpp
+++ ex0_this.cpp
@@ -1,36 +1,29 @@
 #include <iostream>
 
 
-class Foo 
-{
+class Foo {
 public:
-    Foo() 
-    {
+    Foo() {
         this->value = 0;
     }
     
-    Foo get_copy() 
-    {
+    Foo get_copy() {
         return *this;
     }
     
-    Foo& get_copy_as_reference() 
-    {
+    Foo& get_copy_as_reference() {
         return *this;
     }
     
-    Foo* get_pointer() 
-    {
+    Foo* get_pointer() {
         return this;
     }
     
-    void increment() 
-    {
+    void increment() {
         this->value++;
     }
     
-    void print_value() 
-    {
+    void print_value() {
         std::cout << this->value << std::endl;
     }
 private:
@@ -38,8 +31,7 @@ private:
 };
 
 
-int main() 
-{
+int main() {
     Foo foo;
 
     foo.increment();
